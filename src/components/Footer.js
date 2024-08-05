c0c2775884db02f8f7a106bd12ef884372aa3194
sanjay <sanjay529563@gmail.com>
2024-08-06 00:40:33 +0530

    added rebelprotocol

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,9 +7,8 @@ export default function Footer() {
     return (
         <>
             <div className='bg-black/50 w-full  py-6  mx-auto flex items-center justify-center md:justify-between flex-wrap py-2 px-3 sm:px-7 lg:px-28 text-sm  font-medium gap-y-5 border-t-2 border-t-[#FFFFFF1A]' >
+                <p className='text-center'>@2024 Rebel Protocol Ltd. All rights reserved.</p>
                 <div className="flex justify-center md:justify-start">
-                    <p className='hover:text-[#cc3cd9] text-center pr-2 border-r-2 border-r-[#FFFFFF1A]  hover:underline underline-offset-4 '>Privacy Policy</p>
-                    <p className='hover:text-[#cc3cd9] text-center px-2  border-r-2 border-r-[#FFFFFF1A]  hover:underline underline-offset-4 '>Terms And Condition</p>
                     <div className='flex gap-4 flex-col md:flex-row'>
                         <p className='hover:text-[#cc3cd9] text-center pl-2   hover:underline underline-offset-4'>Join With Our Community</p>
                         <div className="flex justify-center items-center space-x-3 md:space-x-5">
@@ -19,7 +18,7 @@ export default function Footer() {
                         </div>
                     </div>
                 </div>
-                <p className='text-center text-[#CC3CD9]'>@RebelProtocol</p>
+
             </div>
         </>
     )
